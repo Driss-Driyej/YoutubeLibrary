27c834da1a816859e7011352405e7e99c3327f30
driyej <login@https://dwarves.iut-fbleau.fr/gitiut/>
2024-05-28 22:38:33 +0200

    Création des fichiers principaux

--- youtube_library/src/App.js
+++ youtube_library/src/App.js
@@ -1,1 +1,12 @@
-App.js
+import React from 'react';
+import LibraryView from './view/LibraryView';
+
+const App = () => {
+  return (
+    <div>
+      <LibraryView />
+    </div>
+  );
+};
+
+export default App;
---
+++ youtube_library/src/view/LibraryView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LibraryView = () => {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+};
+
+export default LibraryView;
